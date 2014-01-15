9858ffae692a8c028b2e14d94ca06669eda439ae
mcgyver5 <tim.e.mcguire@gmail.com>
2014-01-15 17:43:21 -0600

    command line args, csv option, issues identified

--- tools/parse_url.py
+++ tools/parse_url.py
@@ -2,33 +2,92 @@ __author__ = 'mcguit1'
 #Python script to extract URLS and parameters from cumbersome Appscan Report
 import xml.etree.ElementTree as ET
 import sys
+import argparse
+import mimetypes
 
-existingfile = ""
-destfile = ""
-numargs = len(sys.argv)
-if numargs == 3:
-    existfile = sys.argv[1]
-    destfile = sys.argv[2]
-else:
-    print ("needs two params: the name of an existing text file and the name of the destination file")
-    exit()
 
-f = open(existfile)
 
-count = 0
-flist = f.readlines()
-f.close()
-dest = open(destfile,'w')
+def flawed():
+    try:
+        x = 12
+        raise ValueError("sucks to be here")
+    except ValueError:
+        print("yeah I know it is error")
 
-for idx, line in enumerate(flist):
+def about():
+    return "parse_url"
 
-    if line.find('URL:') > -1:
-        url = flist[idx].strip() + " " + flist[idx+1]
-        #urlv = flist[idx +2]
-        print flist[idx].strip() + " " + flist[idx+1]
+def writeLine(destfile,outformat, issue,url,param):
+    if outformat == 'csv':
+        destfile.write(issue + "|" + url + "|" + param)
+        print(issue.strip() + "|" + url.strip() + "|" + param)
+    else:
+        destfile.write("URL: " + url)
+        destfile.write("Parameter: " + param)
 
-        dest.write(url)
-    if line.find('Entity:') > -1:
-        entity = flist[idx].strip() + " " + flist[idx+1]
-        print flist[idx].strip() + " " + flist[idx+1]
-        dest.write(entity)
+def parse_urls(existfile, destfile,outformat):
+    # if it says TOC and then issue, get the issue behind it (idx -1)
+    # if it says TOC and then something else, ignore the something else.
+    print("trying " + existfile)
+    try:
+        with open(existfile):
+            issue = ""
+            f = open(existfile)
+            finishedLine = False
+            flist = f.readlines()
+            f.close()
+            dest = open(destfile,'w')
+
+            countUrl = 0
+            countFileLines = 0
+            url = ""
+            entity = ""
+            for idx, line in enumerate(flist):
+               # print(8)
+                countFileLines = countFileLines + 1
+
+                if line.find('TOC') > -1:
+                    #print(9)
+                    nextLine = flist[idx + 1]
+                    if nextLine.find('Issue') > -1:
+                        issue = flist[idx -1]
+                        # break issue:
+                        rinx = issue.rfind(" ")
+                        issue = issue[0:rinx]
+                        #issue = issueParts[0]
+
+
+                if line.find('URL:') > -1:
+
+                    url = flist[idx+1]
+
+                if line.find('Entity:') > -1:
+                    
+                    entity = flist[idx+1]
+                    finishedLine = True
+                    entityParts = entity.split()
+                    entity = entityParts[0]
+                    spare = entityParts[1]
+
+                if finishedLine:
+                    writeLine(dest,outformat,issue,url,entity)
+                    finishedLine = False
+
+    except IOError as e:
+        print e
+    except TypeError as te:
+        print te
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+
+
+if __name__ == "__main__":
+    # this part only works if called from command line:
+    print("appscan sucks!!!")
+    parser = argparse.ArgumentParser(description="hash incoming text")
+    parser.add_argument('--file','-f', required=True)
+    parser.add_argument('--dest','-d',default='dest.csv')
+    parser.add_argument('--outformat','-o', default='csv')
+    args = parser.parse_args()
+
+    parse_urls(args.file,args.dest,args.outformat)
